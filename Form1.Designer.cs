5891916bdab2dcb2ff0279a1e78dde337b44cb40
Ruben Buleandra <rbuleandra.rb@gmail.com>
2022-10-21 09:17:02 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Buffon_s_Needle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(16, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1371, 738);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(16, 801);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 74);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Desen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(16, 761);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 32);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "10";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(404, 761);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(232, 32);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(404, 801);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(232, 32);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(404, 842);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(232, 32);
+            this.textBox4.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(887, 763);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(136, 31);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Desen linii";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(887, 804);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(225, 31);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Culori la întamplare";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(645, 761);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(233, 54);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Şterge tot";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(645, 822);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(233, 54);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Şterge desenul";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(332, 764);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 27);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Total";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(255, 805);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 27);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Intersectează";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(349, 846);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 27);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Pi≈";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(163, 801);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 74);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Rapid";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1404, 889);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Buffon\'s needle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buffon_s_Needle
+{
+    public partial class Form1 : Form
+    {
+        private Random culoare = new Random();
+        private Random rnd = new Random();
+        private Random xrnd = new Random();
+        private Random yrnd = new Random();
+        double L, H;       
+        double u;          
+        double l;          
+        long intersect = 0;  
+        long total = 0;  
+        Graphics desen; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            desen = panel1.CreateGraphics(); 
+            H = panel1.Height;
+            L = panel1.Width;
+        }
+
+        void desen_linii() 
+        {
+            Pen p = new Pen(Color.Black, 2); 
+            u = H / 3;  
+            int x1, x2, y1, y2; 
+            x1 = 0; 
+            y1 = (int)u;
+            x2 = (int)L;
+            y2 = (int)u;
+            desen.DrawLine(p, x1, y1, x2, y2);  
+            x1 = 0;
+            y1 = 2 * (int)u;
+            x2 = (int)L;
+            y2 = 2 * (int)u;
+            desen.DrawLine(p, x1, y1, x2, y2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            u = H / 3;
+            l = H / 6;  
+            long i, n; 
+            n = Convert.ToInt64(textBox1.Text); 
+            double unghi, sin, cos; 
+            double xc, yc, x1, x2, y1, y2; 
+            Pen p1 = new Pen(Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256)), 2);
+            Pen p2 = new Pen(Color.Red, 2);
+            Pen p3 = new Pen(Color.Black, 2);
+            for (i = 1; i <= n; i++, total++)
+            {
+                xc = rnd.NextDouble() * (L - 2 * l) + l;
+                yc = rnd.NextDouble() * (H - 2 * l) + l;
+                unghi = rnd.NextDouble() * 3.14159265359;
+                cos = Math.Cos(unghi);
+                sin = Math.Sin(unghi);
+                x1 = xc + l * cos;
+                y1 = yc + l * sin;
+                x2 = xc + l * -cos;
+                y2 = yc + l * -sin;
+                if ((y2 <= u && y1 >= u) || (y2 <= 2 * u && y1 >= 2 * u))
+                {
+                    intersect++;
+                    textBox2.Text = Convert.ToString(total);
+                    textBox3.Text = Convert.ToString(intersect);
+                    textBox4.Text = Convert.ToString(2.0 * total / intersect);
+                    Application.DoEvents();
+                    if (checkBox1.Checked == true)
+                        if (checkBox2.Checked == true)
+                        {
+                            p1.Color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                            desen.DrawLine(p1, (int)x1, (int)y1, (int)x2, (int)y2);
+                        }
+                        else
+                            desen.DrawLine(p3, (int)x1, (int)y1, (int)x2, (int)y2);
+                }
+                else 
+                    if (checkBox1.Checked == true)
+                        if (checkBox2.Checked == true)
+                        {
+                            p1.Color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                            desen.DrawLine(p1, (int)x1, (int)y1, (int)x2, (int)y2); 
+                        }
+                        else
+                            desen.DrawLine(p2, (int)x1, (int)y1, (int)x2, (int)y2); 
+            }
+            textBox2.Text = Convert.ToString(total);
+            textBox3.Text = Convert.ToString(intersect);
+            textBox4.Text = Convert.ToString(2.0 * total / intersect);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            desen.Clear(Color.White);
+            desen_linii();
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            total = 0;
+            intersect = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            desen.Clear(Color.White);
+            desen_linii();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            u = H / 3;
+            l = H / 6;
+            long i, n;
+            n = Convert.ToInt64(textBox1.Text);
+            double unghi, sin, cos;
+            double xc, yc, x1, x2, y1, y2;
+            Pen p1 = new Pen(Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256)), 2);
+            Pen p2 = new Pen(Color.Red, 2);
+            Pen p3 = new Pen(Color.Black, 2);
+            for (i = 1; i <= n; i++)
+            {
+                xc = rnd.NextDouble() * (L - 2 * l) + l;
+                yc = rnd.NextDouble() * (H - 2 * l) + l;
+                unghi = rnd.NextDouble() * 3.14159265359;
+                cos = Math.Cos(unghi);
+                sin = Math.Sin(unghi);
+                x1 = xc + l * cos;
+                y1 = yc + l * sin;
+                x2 = xc + l * -cos;
+                y2 = yc + l * -sin;
+                if ((y2 <= u && y1 >= u) || (y2 <= 2 * u && y1 >= 2 * u))
+                    intersect++;
+            }
+            total = total + n;
+            textBox2.Text = Convert.ToString(total);
+            textBox3.Text = Convert.ToString(intersect);
+            textBox4.Text = Convert.ToString(2.0 * total / intersect);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            desen_linii();
+        }
+    }
+}
